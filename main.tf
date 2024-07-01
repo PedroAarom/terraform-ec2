ef96f19b3209e6045072e16118d73f0a30c29866
EC2 Default User <ec2-user@ip-172-31-74-138.ec2.internal>
2024-07-01 13:17:22 +0000

    practica2

--- main.tf
+++ main.tf
@@ -1,17 +1,51 @@
-module "vpc" {
-    source = "./modules/vpc"
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.prefix}-vpc"
+  }
+}
+locals {
+  subnet_count = min(2, 3, 4)
+}
+resource "aws_subnet" "subnets" {
+    count = local.subnet_count
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  tags = {
+    Name = "${var.prefix}-subnet-${count.index}"
+  }
 }
 
-module "compute" {
-    source = "./modules/compute"
-    myvpc = module.vpc.vpc_id
-    subnet = module.vpc.subnet_id
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
-output "subnet_id" {
-  description = "ID of the public subnet"
-  value       = module.vpc.subnet_id
+ 
+output "subnet_ids" {
+  value = join(", ", aws_subnet.subnets.*.id)
 }
 
-output "subnet_names" {
-    value = module.vpc.subnet_names
+resource "aws_instance" "web" {
+  ami           = "ami-01b799c439fd5516a"
+  instance_type = "t2.micro"
+ 
+  tags = {
+    Name = "${var.prefix}-instance"
+    CreatedAt = formatdate("YYYY-MM-DD hh:mm:ss", timestamp())
+  }
+}
+
+resource "aws_subnet" "additional_subnets" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + 3)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index + 3)
+  tags = {
+    Name = "${var.prefix}-additional-subnet-${count.index}"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
